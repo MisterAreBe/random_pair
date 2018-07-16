5d0108adb5a07a0b8de9b0bfd569fc982cd78156
Ricky Collins <richardcollins52317@gmail.com>
2018-07-16 11:42:28 -0400

    pass, test for odd number of names

--- tdd_test.rb
+++ tdd_test.rb
@@ -15,20 +15,25 @@ class TestRandomPair < Minitest::Test
         assert_equal([["Mr.", "Popo"]], pair(["Mr. Popo"]))
     end
 
-    def test_for_2_names
-        assert_equal([["Mr.", "Popo"], ["Goku", "Son"]], pair(["Mr. Popo", "Goku Son"]))
-    end
+    # def test_for_2_names
+    #     assert_equal([["Mr.", "Popo"], ["Goku", "Son"]], pair(["Mr. Popo", "Goku Son"]))
+    # end
 
-    def test_for_4_names
-        names = ["Mr. Popo", "Goku Son", "Prince Vegeta", "King Piccolo"]
-        assert_equal([["Mr.", "Popo"], ["Goku", "Son"], ["Prince", "Vegeta"], ["King", "Piccolo"]], pair(names))
-    end
+    # def test_for_4_names
+    #     names = ["Mr. Popo", "Goku Son", "Prince Vegeta", "King Piccolo"]
+    #     assert_equal([["Mr.", "Popo"], ["Goku", "Son"], ["Prince", "Vegeta"], ["King", "Piccolo"]], pair(names))
+    # end
 
     def test_array_for_random_outcome
         names = ["Tien Shinhan", "Kami Piccolo", "Master Roshi", "Pet Turtle"]
         refute_equal(pair(names), pair(names))
-        p pair(names)
-        p pair(names)
+        p pair(names); p pair(names)
+    end
+
+    def test_array_for_odd_number_of_names
+        names = ["Lord Frieza", "Captain Ginyu", "King Piccolo", "Elder Namik", "Prince Trunks", "Bean Daddy", "Kid Goku"]
+        refute_equal(pair(names), pair(names))
+        p pair(names); p pair(names)
     end
 
 end
